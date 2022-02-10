4e8eb6e360aba6dc188b1e6d8efe32bdaf3ae7be
darrenc-35 <darrenchan35@gmail.com>
2022-02-10 21:15:46 +0800

    added bluetooth functionality

--- app/src/main/java/com/example/androidmdp2022/MainActivity.java
+++ app/src/main/java/com/example/androidmdp2022/MainActivity.java
@@ -1,8 +1,33 @@
 package com.example.androidmdp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import static androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_DRAGGING;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +35,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        }
     }
-}
